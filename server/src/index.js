07a07036727bd77e3c0974d578b8fd4c22d46cd6
Jeremiah Trein <jeremiahtrein@gmail.com>
2018-03-10 22:13:31 +0545

    [server-config-fixes] adds port 4000 as default; moves CORS

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,6 @@
-const cors = require('cors');
-
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
-
-app.use(cors());
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
