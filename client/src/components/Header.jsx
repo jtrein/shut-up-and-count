45533d86117f7697e84c3f59ad1fb47eee0cb45c
Jeremiah Trein <jeremiahtrein@gmail.com>
2018-03-11 01:20:26 +0545

    [client-tests] removes extraneous className from Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/Header.styles';
 import logo from '../img/logo.svg';
 
 const Header = () => (
-  <header className="App-header">
+  <header>
     <img style={styles.logo} src={logo} alt="logo" />
     <h1 style={styles.hide}>Shut Up and Count</h1>
   </header>
