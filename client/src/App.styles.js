6dbc4fcdf0b2ae61c13ea82f950a945316a3c106
Jeremiah Trein <jeremiahtrein@gmail.com>
2018-03-10 18:55:15 +0545

    [dropzone] adds app styles

--- client/src/App.styles.js
+++ client/src/App.styles.js
@@ -1,14 +1,24 @@
 const styles = {
+  logo: {
+    width: 250,
+    maxWidth: '100%',
+  },
+  text: {
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    color: 'deeppink',
+    textAlign: 'center',
+  },
   wrap: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'absolute',
     left: 0,
     right: 0,
     bottom: 0,
     top: 0,
+    marginTop: 24,
   },
 };
 
