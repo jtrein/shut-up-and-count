23c17ab4c55364f8a55c3906ebc803d97e1e8500
Jeremiah Trein <jeremiahtrein@gmail.com>
2018-03-10 18:54:49 +0545

    [dropzone] updates App structure; removes lint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
+import BigButton from './BigButton';
+import FileDropzone from './FileDropzone';
 import logo from './logo.svg';
 import styles from './App.styles';
 import commonStyles from './common.styles';
-import BigButton from './BigButton';
 
 const App = () => (
   <div style={styles.wrap}>
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img style={styles.logo} src={logo} className="App-logo" alt="logo" />
       <h1 style={commonStyles.hide}>Shut Up and Count</h1>
     </header>
 
+    <FileDropzone />
     <BigButton disabled={false} onClick={() => {}} />
   </div>
 );
