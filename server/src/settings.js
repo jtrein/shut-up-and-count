53c16a0f020f35c1b302f648a25ded69d4fa8c7a
Jeremiah Trein <jeremiahtrein@gmail.com>
2018-03-10 13:45:33 +0545

    [parse-logic] adds ALLOWED_FILE_MIME and makes IS_DEV a func in settings

--- server/src/settings.js
+++ server/src/settings.js
@@ -1,5 +1,6 @@
 module.exports = {
-  IS_DEV: process.env.NODE_ENV === 'development',
+  ALLOWED_FILE_MIME: 'text/plain',
+  IS_DEV: () => process.env.NODE_ENV === 'development',
   FILE_KEY: 'textFile',
   MAX_FILE_SIZE: 10 * 1024 * 1024,
   QUERY_EACH: 'each',
