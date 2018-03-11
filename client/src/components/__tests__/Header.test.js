1c649e9cbd6afd4679d3d0d970b2747d7956c241
Jeremiah Trein <jeremiahtrein@gmail.com>
2018-03-11 11:00:41 +0545

    [snapshots] Header snapshot added

--- client/src/components/__tests__/Header.test.js
+++ client/src/components/__tests__/Header.test.js
@@ -1,8 +1,9 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
-import Enzyme, { mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import toJSON from 'enzyme-to-json';
 
 import Header from '../Header';
 
@@ -15,13 +16,18 @@ test('renders without crashing', () => {
 });
 
 test('renders expected initial components', () => {
-  const wrapper = mount(<Header />);
+  const wrapper = shallow(<Header />);
   expect(wrapper.find('header').length).toBe(1);
   expect(wrapper.find('img').length).toBe(1);
   expect(wrapper.find('h1').length).toBe(1);
 });
 
 test('h1 renders expected text', () => {
-  const wrapper = mount(<Header />);
+  const wrapper = shallow(<Header />);
   expect(wrapper.find('h1').text()).toBe('Shut Up and Count');
+});
+
+test('Snapshot: renders as expected', () => {
+  const wrapper = shallow(<Header onClick={() => {}} />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
 });
