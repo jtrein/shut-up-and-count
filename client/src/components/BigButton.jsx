8895a42225e3561ef9090e6ac457b4563eae8e44
Jeremiah Trein <jeremiahtrein@gmail.com>
2018-03-12 16:07:04 +0545

    [big-button-reuse] adds text prop to BigButton

--- client/src/components/BigButton.jsx
+++ client/src/components/BigButton.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { bool, func } from 'prop-types';
+import { bool, func, string } from 'prop-types';
 
 import '../styles/button.css';
 import styles from '../styles/BigButton.styles';
 
-const BigButton = ({ disabled = false, onClick }) => (
+const BigButton = ({ disabled = false, onClick, text }) => (
   <div style={styles.wrap}>
-    <button disabled={disabled} onClick={onClick}>Count the words.</button>
+    <button disabled={disabled} onClick={onClick}>{text}</button>
   </div>
 );
 
 BigButton.defaultProps = {
   disabled: false,
+  text: 'Button',
 };
 
 BigButton.propTypes = {
   disabled: bool,
   onClick: func.isRequired,
+  text: string,
 };
 
 export default BigButton;
